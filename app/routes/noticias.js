d6118008a80eddc877453c540fb7bc436da747ce
Fabricio Araujo <fabricio.info2@gmail.com>
2021-12-21 15:26:04 -0300

    mvc and views refactoring

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,11 +1,11 @@
-module.exports = function(app) {
+module.exports = function(application) {
     
-    app.get('/noticias', function(req, res){
-
-        var connection = app.config.dbConnection();
-
-        connection.query('select * from noticias', function(error, result){
-            res.render("noticias/noticias", { noticias : result });
-        });
+    application.get('/noticias', function(req, res){
+        application.app.controllers.noticias.noticias(application, req, res);
     });
+
+    application.get('/noticia', function(req, res){
+        application.app.controllers.noticias.noticia(application, req, res);
+    });
+
 };
